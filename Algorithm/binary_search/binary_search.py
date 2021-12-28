b67206ab91cbc48df418ba81916521c7516bd869
weihe13 <whe13@dons.usfca.edu>
2021-12-27 19:05:10 -0800

    Success

---
+++ Algorithm/binary_search/binary_search.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        # m = (left + right)//2 如果大于2**31-1，在C语言和java中会报错
+        # 如果while left<right,：
+        # 1. [5] 5, 会return -1
+        # 2. 取不到list的最后一个数：4+(5-4)//2 == 4
+
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            m = left + (right - left) // 2
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                left += 1
+            elif nums[m] > target:
+                right -= 1
+        return -1
